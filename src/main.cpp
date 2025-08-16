3521ba8efb8b519e3e7af7439f342c8e0a5807e2
DugarRishab <your_email@example.com>
2025-08-16 23:37:56 +0530

    [BUG]: fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 #include <ctime>   // time_t, time()
 #include <iomanip> // for std::quoted
 
-#include <headers/telemetry.h>
+#include "headers/telemetry.h"
 
 using namespace std;
 using namespace chrono;
